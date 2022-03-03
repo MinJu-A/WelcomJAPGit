cc4cef1989549527d0cff200de8d3c44d4fcd87e
MinJu-A <pinokoiandmj@naver.com>
2022-03-03 17:15:08 +0900

    Git허브 강의용 test!

---
+++ src/welcome/WelcomJAPGit.java
@@ -0,0 +1,15 @@
+package welcome;
+
+public class WelcomJAPGit {
+
+	public static void main(String[] args) {
+//		다음 깃허브 강의 때 이 화면 다 불러왔는지 확인해보겠습니다!
+//		주석 다 지우지 말고 고대로 놔두세용!
+//		깃허브 강의 후 과제 미리 스포하자면 여기에 각자 이름 다 깃에 올리게 할 거예요 
+//		한 프로젝트에 모두의 이름이 들어갈 수 있게!
+		System.out.println("welcom JAP Git ❤️");
+		System.out.println("나는 202045101 김민주");
+
+	}
+
+}
