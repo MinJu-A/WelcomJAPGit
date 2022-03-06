8a5b06d2b370be908a9ce4fe1ed5adee2078c9d5
MinJu-A <pinokoiandmj@naver.com>
2022-03-07 01:55:03 +0900

    유진이는 보아라

--- src/welcome/WelcomJAPGit.java
+++ src/welcome/WelcomJAPGit.java
@@ -7,8 +7,10 @@ public class WelcomJAPGit {
 //		주석 다 지우지 말고 고대로 놔두세용!
 //		깃허브 강의 후 과제 미리 스포하자면 여기에 각자 이름 다 깃에 올리게 할 거예요 
 //		한 프로젝트에 모두의 이름이 들어갈 수 있게!
-		System.out.println("welcom JAP Git ❤️");
+		System.out.println("Welcom JAP Git ❤️");
 		System.out.println("나는 202045101 김민주");
+		
+//		아주 살짝의 수정을 하였다 메롱메롱 허유진
 	}
 
 }
