1a50ffb787d31be2fe8731e1a5b74168e0456e35
MinJu-A <pinokoiandmj@naver.com>
2022-03-09 00:15:17 +0900

    ㅇㄻ4ㅅ2ㅁㄷㅈㄺㅎㅅ놋ㅈㄷㄺㅎㅅ

--- src/welcome/WelcomJAPGit.java
+++ src/welcome/WelcomJAPGit.java
@@ -6,21 +6,15 @@ package welcome;
 public class WelcomJAPGit {
 
 	public static void main(String[] args) {
-//		다음 깃허브 강의 때 이 화면 다 불러왔는지 확인해보겠습니다!
-//		주석 다 지우지 말고 고대로 놔두세용!
-//		깃허브 강의 후 과제 미리 스포하자면 여기에 각자 이름 다 깃에 올리게 할 거예요 
-//		한 프로젝트에 모두의 이름이 들어갈 수 있게!
 		System.out.println("Welcom JAP Git ❤️");
 		System.out.println("나는 202045101 김민주");
 		System.out.println("test");
 		
-//		브랜치 만들기-충돌생성용으로 미리 만들었어요
-//		충돌을 시켜볼게요
-		
+
 		
 
 		
-		
+//	뭐가 문제인 것인가	
 		
 	}
 
